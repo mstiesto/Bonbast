0b4de80f36667f2ff93a3ed58b289ed51176ca05
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-01 15:44:47 +0330

    improvement

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os, telebot, requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
@@ -8,6 +9,9 @@ BOT = telebot.TeleBot(BOT_TOKEN)
 
 @BOT.message_handler(commands=['start', 'hello'])
 def send_price(message):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
     url     = 'https://bonbast.com/'
     driver  = webdriver.PhantomJS()
     driver.get(url)
