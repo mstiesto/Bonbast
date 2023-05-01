99743becca6cea34b0dd3febfd4e147f6d70cb35
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-01 10:52:06 +0330

    add docker compose

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 import os, telebot, requests
 from bs4 import BeautifulSoup
 
-
-url = 'https://bonbast.com'
-html_text = requests.get(url).text
-soup = BeautifulSoup(html_text, 'html.parser')
-
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
 
 @BOT.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    BOT.reply_to(message, soup)
+def send_price(message):
+    url = 'https://bonbast.com'
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    euro = soup.find(id="eur1")
+    BOT.reply_to(message, euro)
 BOT.infinity_polling()
