36b9aaca61c27958f5bf3daf38cb0f00d7463f91
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-05 17:28:08 +0330

    change script

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ BOT = telebot.TeleBot(BOT_TOKEN)
 def send_price(message):
     url = 'https://bonbast.com/'
     dryscrape.start_xvfb()
-    dryscrape.st
     session = dryscrape.Session()
     session.set_header('user-agent', 'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36')
     session.set_attribute('auto_load_images', False)
