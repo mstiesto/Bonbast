3852dc6972d6317e407cbdd6fe08af180ad41d57
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-05 14:30:28 +0330

    change script

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import os, telebot, dryscrape
+import os, telebot
+from playwright.sync_api import Playwright, sync_playwright
 from bs4 import BeautifulSoup
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -7,17 +8,32 @@ BOT = telebot.TeleBot(BOT_TOKEN)
 
 @BOT.message_handler(commands=['euro', 'dollar'])
 def send_price(message):
-    url = 'https://bonbast.com/'
-    dryscrape.start_xvfb()
-    session = dryscrape.Session()
-    session.visit(url)
-    response = session.body()
-    soup = BeautifulSoup(response, features="lxml")
-    if message.text == "/euro":
-        currency = soup.find(id="eur1")
-    elif message.text == "/dollar":
-        currency = soup.find(id="usd1")
-    else:
-        BOT.reply_to(message, "Could not find the currency")
-    BOT.reply_to(message, currency.prettify())
-BOT.infinity_polling()
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+
+        # Load the web page
+        page.goto('https://bonbast.com')
+
+        # Perform actions to load dynamic content here
+        # ...
+
+        # Get the HTML content of the page
+        html = page.content()
+
+        # Extract the data you want using a Python HTML parsing library like BeautifulSoup or lxml
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Extract data from the soup object here
+        if message.text == "/euro":
+            currency = soup.find(id="eur1")
+        elif message.text == "/dollar":
+            currency = soup.find(id="usd1")
+        else:
+            BOT.reply_to(message, "Could not find the currency")
+        BOT.reply_to(message, currency.prettify())
+        # Close the browser
+        browser.close()
+
+    BOT.infinity_polling()
