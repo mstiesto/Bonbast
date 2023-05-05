20f995ea2fb537f8278108a0f5089000c74be0ba
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-05 13:25:00 +0330

    update actions and script

--- bot.py
+++ bot.py
@@ -1,20 +1,23 @@
-import os, telebot, requests
+import os, telebot, dryscrape
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
 
 
-@BOT.message_handler(commands=['start', 'hello'])
+@BOT.message_handler(commands=['euro', 'dollar'])
 def send_price(message):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-    url     = 'https://bonbast.com/'
-    driver  = webdriver.PhantomJS()
-    driver.get(url)
-    euro = driver.find_element_by_id(id_='euro1')
-    BOT.reply_to(message, euro)
+    url = 'https://bonbast.com/'
+    dryscrape.start_xvfb()
+    session = dryscrape.Session()
+    session.visit(url)
+    response = session.body()
+    soup = BeautifulSoup(response, features="lxml")
+    if message.text == "/euro":
+        currency = soup.find(id="eur1")
+    elif message.text == "/dollar":
+        currency = soup.find(id="usd1")
+    else:
+        BOT.reply_to(message, "Could not find the currency")
+    BOT.reply_to(message, currency.prettify())
 BOT.infinity_polling()
