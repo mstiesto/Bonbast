b39641201cd793448603100a8c7ee6e0dc1c3578
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-05 17:34:27 +0330

    change script

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ def send_price(message):
     session = dryscrape.Session()
     session.set_header('user-agent', 'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36')
     session.set_attribute('auto_load_images', False)
-    session.set_attribute('auto_load_css', False)
     session.set_timeout(30)
     session.visit(url)
     response = session.body()
