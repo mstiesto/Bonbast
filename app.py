141babb89c88eca76e4f9b65034bcafb94ec384a
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-06 15:42:26 +0330

    change script

--- app.py
+++ app.py
@@ -2,31 +2,43 @@ import os, telebot, time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from pymemcache.client import base
+
+client = base.Client(('memcached', 11211))
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
-
 @BOT.message_handler(commands=['euro', 'dollar'])
 
-def send_price(message):
+def get_price():
+    currencies = ['eur1', 'eur2', 'usd1', 'usd2']
     url = 'https://bonbast.com/'
     options = webdriver.ChromeOptions()
     options.add_argument('--headless=new')
     options.add_argument("window-size=1920x1080")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
-    # options.add_argument('--disable-dev-shm-usage') # Not used 
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     time.sleep(5)
+    driver.close()
     src = driver.page_source
     soup = BeautifulSoup(src)
+    for currency in currencies:
+        price = soup.find(id=currency)
+        client.set(currency, price)
+def send_price(message):
     if message.text == "/euro":
-        currency = soup.find(id="eur1")
+        currency = client.get('eur1')
     elif message.text == "/dollar":
-        currency = soup.find(id="usd1")
+        currency = client.get('usd1')
     else:
-        BOT.reply_to(message, "Could not find the currency")
+        BOT.reply_to(message, "Could not find the currency")    
     BOT.reply_to(message, currency)
-    driver.close()
+
+
 BOT.infinity_polling()
+
+while True:
+    get_price()
+    time.sleep(1800)
