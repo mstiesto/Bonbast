7da2cff1f394f4fb146bc93b2b12806fba4341fe
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-06 16:28:03 +0330

    change script

--- app.py
+++ app.py
@@ -7,25 +7,6 @@ from pymemcache.client import base
 client = base.Client(('memcached', 11211))
 currencies = ['eur1', 'eur2', 'usd1', 'usd2']
 
-url = 'https://bonbast.com/'
-options = webdriver.ChromeOptions()
-options.add_argument('--headless=new')
-options.add_argument("window-size=1920x1080")
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(options=options)
-
-while true:
-    driver.get(url)
-    time.sleep(5)
-    driver.close()
-    src = driver.page_source
-    soup = BeautifulSoup(src)
-    for currency in currencies:
-        price = soup.find(id=currency)
-        client.set(currency, price)
-    time.sleep(1800)
-
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
 @BOT.message_handler(commands=['euro', 'dollar'])
@@ -41,23 +22,21 @@ def send_price(message):
 
 BOT.infinity_polling()
 
+url = 'https://bonbast.com/'
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=new')
+options.add_argument("window-size=1920x1080")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)
 
-
-
-
-
-
-
-
-
-
-
-def send_price(message):
-    if message.text == "/euro":
-        currency = client.get('eur1')
-    elif message.text == "/dollar":
-        currency = client.get('usd1')
-    else:
-        BOT.reply_to(message, "Could not find the currency")    
-    BOT.reply_to(message, currency)
-BOT.infinity_polling()
+while True:
+    driver.get(url)
+    time.sleep(5)
+    driver.close()
+    src = driver.page_source
+    soup = BeautifulSoup(src)
+    for currency in currencies:
+        price = soup.find(id=currency)
+        client.set(currency, price)
+    time.sleep(1800)
