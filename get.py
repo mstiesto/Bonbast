168f7afcbc1b5705972c1507ff95ca2c2fdc55d3
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-07 13:58:40 +0330

    change script

--- get.py
+++ get.py
@@ -2,19 +2,16 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from pymemcache.client import base
-
 client = base.Client(('memcached', 11211))
 currencies = ['eur1', 'eur2', 'usd1', 'usd2']
-
-url = 'https://bonbast.com/'
-options = webdriver.ChromeOptions()
-options.add_argument('--headless=new')
-options.add_argument("window-size=1920x1080")
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(options=options)
-
 while True:
+    url = 'https://bonbast.com/'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless=new')
+    options.add_argument("window-size=1920x1080")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
     print("Geting price list ...")
     driver.get(url)
     time.sleep(5)
