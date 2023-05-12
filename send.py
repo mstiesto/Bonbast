4bcd763dcc2beb095e18c9f9f81552866ef900ae
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-12 20:02:48 +0330

    change script

--- send.py
+++ send.py
@@ -4,17 +4,32 @@ from pymemcache.client import base
 client = base.Client(('memcached', 11211))
 currencies = ['eur1', 'eur2', 'usd1', 'usd2']
 
+currencies = {
+    "euro" : {
+        "name" : "euro",
+        "buyID" : "eur2",
+        "sellID" : "eur1"
+    },
+    "dollar" : {
+        "name" : "dollar",
+        "buyID" : "usd2",
+        "sellID" : "usd1"
+    }
+}
+
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
-@BOT.message_handler(commands=['euro', 'dollar'])
+@BOT.message_handler(commands=list(currencies.keys()))
 def send_price(message):
     if message.text == "/euro":
-        currency = client.get('eur1')
+        sell = client.get('eur1')
+        buy = client.get('eur2')
     elif message.text == "/dollar":
-        currency = client.get('usd1')
+        sell = client.get('usd1')
+        buy = client.get('usd2')
     else:
         BOT.reply_to(message, "Could not find the currency")    
-    BOT.reply_to(message, currency)
+    BOT.reply_to(message, "buy:", buy, "sell:", sell)
 
 
 BOT.infinity_polling()
