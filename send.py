d274c9337471f131e63d1095ebb3bec558cf703e
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-14 01:29:17 +0330

    change script

--- send.py
+++ send.py
@@ -1,21 +1,29 @@
 import os, telebot
 from pymemcache.client import base
-
 client = base.Client(('memcached', 11211))
-currencies = ['eur1', 'eur2', 'usd1', 'usd2']
-
+# currencies = ['eur1', 'eur2', 'usd1', 'usd2']
+currencies = {
+    "euro" : {
+        "name" : "euro",
+        "buyID" : "eur2",
+        "sellID" : "eur1"
+    },
+    "dollar" : {
+        "name" : "dollar",
+        "buyID" : "usd2",
+        "sellID" : "usd1"
+    }
+}
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
-@BOT.message_handler(commands=["euro", "dollar"])
+@BOT.message_handler(commands=list(currencies.keys()))
 def send_price(message):
-    if message.text == "/euro":
-        sell = client.get('eur1')
-        buy = client.get('eur2')
-    elif message.text == "/dollar":
-        sell = client.get('usd1')
-        buy = client.get('usd2')
-    else:
-        BOT.reply_to(message, "Could not find the currency")
+    for currency in currencies.values():
+        if message.text == "/" + currency['name']:
+            sell = client.get(currency['sellID'])
+            buy = client.get(currency['buyID'])
+        else:
+            BOT.reply_to(message, "Could not find the currency")
     text = ("Sell: " + str(sell, 'utf-8') + "\n" + "Buy: " + str(buy, 'utf-8'))
     BOT.reply_to(message, text)
 BOT.infinity_polling()
