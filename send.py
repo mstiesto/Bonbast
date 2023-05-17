1f99f7d23b27cacdc1f3931e6b4ed872a42b99e5
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-17 13:28:14 +0330

    change script

--- send.py
+++ send.py
@@ -3,8 +3,8 @@ from pymemcache.client import base
 client = base.Client(('memcached', 11211))
 with open("objects.yaml") as o:
     objects = yaml.load(o, Loader=yaml.FullLoader)
-    currencies = objects['currencies']
-    coins = objects['coins']
+    # currencies = objects['currencies']
+    # coins = objects['coins']
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT = telebot.TeleBot(BOT_TOKEN)
 @BOT.message_handler(commands=['start'])
@@ -13,9 +13,9 @@ def start(message):
     for object in objects.keys():
         items = items + "/" + object + "\n"
     BOT.reply_to(message, items)
-@BOT.message_handler(commands=list(currencies.keys()))
+@BOT.message_handler(commands=list(objects['currencies'].keys()))
 def send_price(message):
-    for currency, ids in currencies.items():
+    for currency, ids in objects['currencies'].items():
         if message.text == "/" + currency:
             sell = client.get(ids['sellID'])
             buy = client.get(ids['buyID'])
