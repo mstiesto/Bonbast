500b33afa2f3adbbdf9f438ecdd8882269f16211
Pouria Shahmohammadi <pouria.shahmohammadi@snapp.cab>
2023-05-17 12:52:12 +0330

    change script

--- send.py
+++ send.py
@@ -13,8 +13,10 @@ def start(message):
     for object in objects.keys():
         items = items + "/" + object + "\n"
     BOT.reply_to(message, items)
-# @BOT.message_handler(commands=['currencies', 'coins'])
-# def list(message):
+@BOT.message_handler(commands=['currencies', 'coins'])
+def list(message):
+    if message.text == "currencies":
+        BOT.reply_to(message, "salam")
 #     items = ""
 #     for k, v in objects:
 #         if message.text == k:
