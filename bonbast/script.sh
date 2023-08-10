42b3241d05c151717e9610e5f0ffd7e056a65f84
Morteza Shahmohammadi <53808802+mstiesto@users.noreply.github.com>
2023-08-10 08:13:09 +0200

    Create script.sh

---
+++ bonbast/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ls -ltr
+buildah login -u "$DOCKER_USER" -p "$DOCKER_TOKEN" docker.io
+# Create an empty array
+array=()
+# Use a while loop to read each line of the string
+while IFS= read -r line; do
+array+=("$line")  # Append each line to the array
+done <<< "$(params.CHANGED_FOLDERS)"
+
+for folder in "${array[@]}"; do
+  echo "Processing folder: $folder"
+
+  FOLDER_PATH=./$folder
+  IMAGE_URL=$(params.IMAGE)/$folder:$(params.DOCKER_TAG)
+
+  buildah --storage-driver=$(params.STORAGE_DRIVER) bud -f $FOLDER_PATH/Dockerfile -t $IMAGE_URL $FOLDER_PATH && \
+  buildah --storage-driver=$(params.STORAGE_DRIVER) push $IMAGE_URL
+done
