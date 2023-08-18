5387fd829b7046589089d188bf8e2ac1af2fe54c
mstiesto <mstiesto01@gmail.com>
2023-08-18 10:56:34 +0200

    Ädd test file

--- bonbast/test.sh
+++ bonbast/test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mapfile -t CHANGES < <$CHANGED_FOLDERS
-IFS=' ' read -r CHANGES < <(echo "$CHANGED_FOLDERS")
+mapfile -t CHANGES < <(echo $CHANGED_FOLDERS)
 for FOLDER in "$CHANGES"
 do
   echo "Processing folder: $FOLDER"
